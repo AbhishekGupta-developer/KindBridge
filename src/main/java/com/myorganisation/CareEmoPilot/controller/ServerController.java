50a7b8ce20c521db3b3034ae2b95479747dab8de
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-06-10 20:53:31 +0530

    Create Server status API

---
+++ src/main/java/com/myorganisation/CareEmoPilot/controller/ServerController.java
@@ -0,0 +1,30 @@
+package com.myorganisation.CareEmoPilot.controller;
+
+import com.myorganisation.CareEmoPilot.dto.ServerStatusResponseDTO;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+
+@RestController
+@RequestMapping("/")
+public class ServerController {
+
+    private final Instant serverStartTime;
+    private final String applicationName;
+
+    public ServerController(@Value("${spring.application.name}") String applicationName) {
+        this.serverStartTime = Instant.now();
+        this.applicationName = applicationName;
+    }
+
+    @GetMapping
+    public ResponseEntity<ServerStatusResponseDTO> serverStatus() {
+        System.out.println("Hellooooo");
+        return new ResponseEntity<>(new ServerStatusResponseDTO(serverStartTime, applicationName), HttpStatus.OK);
+    }
+}
