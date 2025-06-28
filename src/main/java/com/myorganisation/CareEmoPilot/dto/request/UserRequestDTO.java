eb0a0c341b62b95a7500aaffe08fdc12ce9c2f6b
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-06-28 21:44:34 +0530

    Add validation message

--- src/main/java/com/myorganisation/CareEmoPilot/dto/request/UserRequestDTO.java
+++ src/main/java/com/myorganisation/CareEmoPilot/dto/request/UserRequestDTO.java
@@ -18,7 +18,7 @@ public class UserRequestDTO {
     private String lastName;
 
     @NotBlank(message = "Username is required")
-    @Size(min = 4, max = 20)
+    @Size(min = 4, max = 20, message = "Username must be between 4 and 20 characters")
     private String username;
 
     @Email(message = "Email must be valid")
