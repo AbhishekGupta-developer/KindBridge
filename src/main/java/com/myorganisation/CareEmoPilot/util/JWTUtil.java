00a04b5f0cbe819b9d353322a1cac6c17ee7499c
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-07-16 21:33:00 +0530

    Implement JWTUtil to generate JWT token

--- src/main/java/com/myorganisation/CareEmoPilot/util/JWTUtil.java
+++ src/main/java/com/myorganisation/CareEmoPilot/util/JWTUtil.java
@@ -1,7 +1,26 @@
 package com.myorganisation.CareEmoPilot.util;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
 
 @Component
 public class JWTUtil {
+
+    private final String SECRET = "zT9#Fn3@Xe7^Vr!qKpL2$Wu8Df0*GmYaJv1RxNcBZsQ";
+    private final SecretKey KEY = Keys.hmacShaKeyFor(SECRET.getBytes());
+    private final long EXPIRATION_TIME = 1000 * 60 * 5; //Valid for 5 mins
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(KEY, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
 }
