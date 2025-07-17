006c23890dba77678944b4ac7c61d495215b01f0
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-07-18 01:07:54 +0530

    feat(auth): add JWT-based authentication endpoint

--- src/main/java/com/myorganisation/CareEmoPilot/controller/AuthController.java
+++ src/main/java/com/myorganisation/CareEmoPilot/controller/AuthController.java
@@ -1,15 +1,44 @@
 package com.myorganisation.CareEmoPilot.controller;
 
 import com.myorganisation.CareEmoPilot.dto.request.AuthRequestDTO;
+import com.myorganisation.CareEmoPilot.util.JWTUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/authenticate")
 public class AuthController {
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @PostMapping
+    public ResponseEntity<String> authenticateUser(@RequestBody AuthRequestDTO authRequestDTO) {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            authRequestDTO.getUsername(),
+                            authRequestDTO.getPassword()
+                    )
+            );
+
+            return new ResponseEntity<>(jwtUtil.generateToken(authRequestDTO.getUsername()), HttpStatus.OK);
+        } catch(AuthenticationException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
