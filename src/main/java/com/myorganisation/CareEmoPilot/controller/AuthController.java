1000d6ff971d0a311fc736693e7ae0ab4a34700a
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-07-17 23:57:01 +0530

    Add AuthController

---
+++ src/main/java/com/myorganisation/CareEmoPilot/controller/AuthController.java
@@ -0,0 +1,15 @@
+package com.myorganisation.CareEmoPilot.controller;
+
+import com.myorganisation.CareEmoPilot.dto.request.AuthRequestDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/authenticate")
+public class AuthController {
+
+
+}
