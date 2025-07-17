26caeb111b02c0ae549d646c19935318ea2bad99
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-07-18 01:01:54 +0530

    refactor(security): use AbstractHttpConfigurer::disable for CSRF as per Spring Security 6.1

--- src/main/java/com/myorganisation/CareEmoPilot/config/SecurityConfig.java
+++ src/main/java/com/myorganisation/CareEmoPilot/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -21,6 +22,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         auth -> auth
                                 .requestMatchers(HttpMethod.GET, "/").permitAll()
