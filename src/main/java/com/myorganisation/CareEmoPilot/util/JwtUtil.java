6903e1db06407fd591f28a01e32ab9615301ff01
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-08-01 22:14:21 +0530

    Remove old unnecessary code

--- src/main/java/com/myorganisation/CareEmoPilot/util/JwtUtil.java
+++ src/main/java/com/myorganisation/CareEmoPilot/util/JwtUtil.java
@@ -15,38 +15,6 @@ public class JwtUtil {
     private final SecretKey KEY = Keys.hmacShaKeyFor(SECRET.getBytes());
     private final long EXPIRATION_TIME = 1000 * 60 * 5; //Valid for 5 mins
 
-    public String generateToken(String username) {
-        return Jwts.builder()
-                .setSubject(username)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(KEY, SignatureAlgorithm.HS256)
-                .compact();
-    }
-
-    public String extractUsername(String token) {
-        Claims body = getClaims(token);
-
-        return body.getSubject();
-    }
-
-    private Claims getClaims(String token) {
-        Claims body = Jwts.parser()
-                .setSigningKey(KEY)
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
-        return body;
-    }
-
-    public boolean validateToken(String username, UserDetails userDetails, String token) {
-        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
-    }
-
-    private boolean isTokenExpired(String token) {
-        return getClaims(token).getExpiration().before(new Date());
-    }
-
     //Signup JWT logic here
     public String generateSignupToken(String email) {
         return Jwts.builder()
