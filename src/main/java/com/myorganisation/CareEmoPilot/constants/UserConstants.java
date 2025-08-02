00ba2f452a1c0c1aa55db2ef72595a3ee61aebd2
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-08-03 04:41:12 +0530

    Partial code commit

---
+++ src/main/java/com/myorganisation/CareEmoPilot/constants/UserConstants.java
@@ -0,0 +1,7 @@
+package com.myorganisation.CareEmoPilot.constants;
+
+public final class UserConstants {
+    private UserConstants() {}
+
+    public static final String PASSWORD_NOT_SET = "NOT_SET";
+}
