53800ac42dd24ba8e7359139f40961d8f52be805
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-08-26 23:58:54 +0530

    Handle UserNotFoundException in GlobalExceptionHandler

--- src/main/java/com/myorganisation/KindBridge/exception/GlobalExceptionHandler.java
+++ src/main/java/com/myorganisation/KindBridge/exception/GlobalExceptionHandler.java
@@ -1,7 +1,20 @@
 package com.myorganisation.KindBridge.exception;
 
+import com.myorganisation.KindBridge.dto.response.GenericResponseDto;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<GenericResponseDto> handleUserNotFoundException(UserNotFoundException e) {
+        GenericResponseDto genericResponseDto = new GenericResponseDto();
+        genericResponseDto.setSuccess(false);
+        genericResponseDto.setMessage("User not found");
+
+        return new ResponseEntity<>(genericResponseDto, HttpStatusCode.valueOf(404));
+    }
 }
