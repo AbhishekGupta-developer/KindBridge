c32cf3a4e4f48ceea35b03c8596031179178f605
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-08-27 23:59:45 +0530

    Handle validations exception

--- src/main/java/com/myorganisation/KindBridge/exception/GlobalExceptionHandler.java
+++ src/main/java/com/myorganisation/KindBridge/exception/GlobalExceptionHandler.java
@@ -3,8 +3,12 @@ package com.myorganisation.KindBridge.exception;
 import com.myorganisation.KindBridge.dto.response.GenericResponseDto;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -16,5 +20,22 @@ public class GlobalExceptionHandler {
         genericResponseDto.setMessage("User not found");
 
         return new ResponseEntity<>(genericResponseDto, HttpStatusCode.valueOf(404));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<GenericResponseDto> handleValidationExceptions(MethodArgumentNotValidException e) {
+        GenericResponseDto genericResponseDto = new GenericResponseDto();
+        genericResponseDto.setSuccess(false);
+        genericResponseDto.setMessage(e.getMessage());
+
+        Map<String, String> errors = new HashMap<>();
+
+        e.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        genericResponseDto.setData(errors);
+
+        return new ResponseEntity<>(genericResponseDto, HttpStatusCode.valueOf(400));
     }
 }
