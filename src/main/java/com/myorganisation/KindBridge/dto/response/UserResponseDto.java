516185b3bc353527d15643fd66950e2783936b83
Abhishek Gupta <abhishekg.developer@gmail.com>
2025-10-03 20:50:04 +0530

    Refactor UserResponseDto

--- src/main/java/com/myorganisation/KindBridge/dto/response/UserResponseDto.java
+++ src/main/java/com/myorganisation/KindBridge/dto/response/UserResponseDto.java
@@ -1,7 +1,15 @@
 package com.myorganisation.KindBridge.dto.response;
 
+import com.myorganisation.KindBridge.enums.AreaType;
 import com.myorganisation.KindBridge.enums.RoleType;
+import com.myorganisation.KindBridge.enums.SupporterType;
+import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
@@ -11,13 +19,19 @@ import lombok.*;
 public class UserResponseDto {
 
     private Long id;
+    private String email;
+    private boolean isEmailVerified;
+    private boolean active;
+    private RoleType role;
+    private SupporterType supporterType;
+    private List<AreaType> areas;
     private String firstName;
     private String lastName;
-    private String username;
-    private String email;
     private String phone;
-    private RoleType roleType;
     private boolean anonymous;
+    private boolean isRegistrationCompleted;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
 }
 
